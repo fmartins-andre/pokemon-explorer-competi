f52bfc88af252c09faa8ac2c79186cfbea3501c8
André Martins <fmartins.andre@gmail.com>
2021-10-06 14:13:30 -0300

    Add better behavior to the "add to pokedex" action wrapper

--- components/addToPokedex/AddPokemon.tsx
+++ components/addToPokedex/AddPokemon.tsx
@@ -1,15 +1,30 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, MouseEventHandler } from 'react'
+import { useRouter } from 'next/router'
 import pokedexController from '../../controller/pokedex'
 
 const AddPokemon: FunctionComponent<{name: string}> = props => {
   const { name, children, ...other } = props
+  const router = useRouter()
 
-  const onAddToPokedexClick = () => {
-    console.log(
-      pokedexController().add([name])
-        ? `Added ${name} to your pokedex`
+  const onAddToPokedexClick: MouseEventHandler<HTMLAnchorElement> = (event) => {
+    event.preventDefault()
+    const element = event.target as HTMLAnchorElement
+
+    const pokedex = pokedexController().add([name])
+
+    console.info(
+      pokedex
+        ? `Added ${name} to your pokedex: \n- ${pokedex.join('\n- ')}`
         : `You need to login to add ${name} to your pokedex`
     )
+
+    if (!pokedex) {
+      console.info(`You need to login to add ${name} to your pokedex`)
+      router.push('/session')
+    } else {
+      console.info(`Added ${name} to your pokedex: \n- ${pokedex.join('\n- ')}`)
+      element.style.filter = 'grayscale(0.6)'
+    }
   }
 
   return (
