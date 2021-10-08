b853ef3f0cda9b79d960a8ee75cfa5c57ba34c40
André Martins <fmartins.andre@gmail.com>
2021-10-08 10:34:09 -0300

    Add redux based state management to the filter component

--- components/filter/PokemonFilter.tsx
+++ components/filter/PokemonFilter.tsx
@@ -1,15 +1,29 @@
-import { ChangeEventHandler, FunctionComponent } from 'react'
+import { ChangeEventHandler, FunctionComponent, useState, useEffect } from 'react'
 import styles from './PokemonFilter.module.css'
 
+import queriesStore from '../../redux'
+
 const PokemonFilter: FunctionComponent<{types: string[], onChangeFilter: ChangeEventHandler }> = (props) => {
-  const options = props.types.map((type, index) => <option key={index} value={type}>{type}</option>)
+  const [type, setType] = useState<string>()
+
+  useEffect(setTypeState, [])
+  queriesStore.subscribe(setTypeState)
+
+  function setTypeState () {
+    const state = queriesStore.getState()
+    setType(state.variables.type)
+  }
+
+  const options = props.types.map(
+    (type, index) => <option key={index} value={type}>{type}</option>
+  )
 
   return (
     <div className={styles.container}>
       <div className={styles.formControl}>
 
         <label htmlFor="pokemonType">filter</label>
-        <select name="pokemonType" id="pokemonType" onChange={props.onChangeFilter}>
+        <select name="pokemonType" id="pokemonType" onChange={props.onChangeFilter} value={type}>
           <option value="">-- all --</option>
           {options}
         </select>
